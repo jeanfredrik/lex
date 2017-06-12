69c4d4df396102d95b5929f60bf115c53b6bf929
jeanfredrik <jeanfredrik@gmail.com>
2017-06-12 21:11:23 +0200

    ✨ Add version info in header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import Main from './containers/Main';
 
 const App = () => (
   <div className="flex flex-column height-100">
-    <div className="flex-none bg-black white px2 py1">
-      <span className="uppercase bold h3">Lex</span>
-      <span className="opacity-50 pl1">– Test-drive your phonotactics</span>
+    <div className="flex-none bg-black white px1 py1 flex items-baseline">
+      <div className="flex-auto px1">
+        <span className="uppercase bold h3">Lex</span>
+        <span className="opacity-50 pl1">– Test-drive your phonotactics</span>
+      </div>
+      <div className="flex-none px1">
+        <a
+          href={process.env.REACT_APP_RELEASE_LIST}
+          className="color-inherit opacity-50 no-underline hover-underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {process.env.REACT_APP_VERSION}
+        </a>
+      </div>
     </div>
     <Main />
   </div>
