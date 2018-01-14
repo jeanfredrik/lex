b3c576efa09f9bf67834ab42fd74f5bc6374cb7c
Fredrik Johansson <jeanfredrik@gmail.com>
2018-01-14 10:45:04 +0100

    💬 Update initial and migrated field values

--- src/migrate.js
+++ src/migrate.js
@@ -1,3 +1,4 @@
+import { stripIndent } from 'common-tags'
 import localStorage from './localStorage'
 
 const migrations = [
@@ -15,16 +16,37 @@ const migrations = [
       const { docs } = oldStorage
       if (docs) {
         window.localStorage.clear()
+        const news = stripIndent`
+          # New in version 2.0:
+          # - The two textareas have been merged into one.
+          # - You can mix symbol definitions and word patterns.
+          # - You can assign weights to different patterns in two ways.
+          #   If you want a pattern to be used twice as often:
+          CV*2
+          #   If you want a pattern to be used less:
+          CV*0.5
+          #   Or with division:
+          CV/2
+
+          # - You can make parts of the pattern appear less often by using parentheses.
+          #   This will give CVCV in half of the words and CV in the rest:
+          CV(CV)
+
+          # - It’s also possible to assign weights to those subpatterns:
+          CV(CV*0.25)
+          #   Or:
+          CV(CV/4)
+
+          # - You can add comments by starting the line with a # sign.
+        `
         const newStorage = {
-          docs: docs.map(({
-            patterns,
-            definitions,
-            ...rest
-          }) => ({
-            ...rest,
-            input: `${definitions}\n\n${patterns}`,
-          })),
-        };
+          docs: docs.map(
+            ({ patterns, definitions, ...rest }) => ({
+              ...rest,
+              input: `${definitions}\n\n${patterns}\n\n${news}`,
+            }),
+          ),
+        }
         localStorage.set('jeanfredrik/lex/1', newStorage)
       }
     }
